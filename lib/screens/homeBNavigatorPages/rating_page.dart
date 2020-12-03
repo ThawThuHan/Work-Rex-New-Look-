927b4d22e95abbb9a7143d7865e3a79d1abfc3e6
ThawThuHan <thawthuhan9@gmail.com>
2020-12-03 23:37:26 +0630

    editRatingPage

--- lib/screens/homeBNavigatorPages/rating_page.dart
+++ lib/screens/homeBNavigatorPages/rating_page.dart
@@ -33,128 +33,153 @@ class _RatingPageState extends State<RatingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('** Users Rank **'),
-        elevation: 0.0,
-        backgroundColor: Theme.of(context).accentColor,
-      ),
+      // appBar: AppBar(
+      //   centerTitle: true,
+      //   title: Text('** Users Rank **'),
+      //   elevation: 0.0,
+      //   backgroundColor: Theme.of(context).accentColor,
+      // ),
       body: Container(
+        color: Theme.of(context).accentColor,
         child: Column(
-          // crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            StreamBuilder<List<WorkRexUser>>(
-              stream: streamWorkRexUsers,
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-                List<WorkRexUser> users = snapshot.data;
-                users.sort((a, b) => (b.overall).compareTo(a.overall));
-                return Expanded(
-                  child: ListView.builder(
-                    itemBuilder: (context, index) {
-                      WorkRexUser user = users[index];
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ProfilePage(
-                                user: users[index],
-                                currentUserid: widget.currentUserId,
-                                currentUserName: widget.currentUserName,
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(40.0),
+                // color: Theme.of(context).accentColor,
+                child: Text(
+                  '*** Users Ranking ***',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    )),
+                child: StreamBuilder<List<WorkRexUser>>(
+                  stream: streamWorkRexUsers,
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    List<WorkRexUser> users = snapshot.data;
+                    users.sort((a, b) => (b.overall).compareTo(a.overall));
+                    return ListView.builder(
+                      itemBuilder: (context, index) {
+                        WorkRexUser user = users[index];
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ProfilePage(
+                                  user: users[index],
+                                  currentUserid: widget.currentUserId,
+                                  currentUserName: widget.currentUserName,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            child: Card(
+                              child: Row(
+                                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Expanded(
+                                    flex: 4,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(12.0),
+                                      child: Row(
+                                        children: [
+                                          Text(
+                                            '${index + 1}',
+                                            style: TextStyle(
+                                              fontSize: 24.0,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(
+                                            width: 10.0,
+                                          ),
+                                          user.imgUrl == null
+                                              ? ProfilePlaceholder(radius: 24.0)
+                                              : CustomCircleProfilePic(
+                                                  imgUrl: user.imgUrl,
+                                                  radius: 24.0,
+                                                ),
+                                          SizedBox(
+                                            width: 20.0,
+                                          ),
+                                          Column(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                user.name,
+                                                style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 18.0,
+                                                ),
+                                              ),
+                                              SmoothStarRating(
+                                                size: 25,
+                                                rating: user.overall,
+                                                isReadOnly: true,
+                                                filledIconData: Icons.star,
+                                                halfFilledIconData:
+                                                    Icons.star_half,
+                                                defaultIconData:
+                                                    Icons.star_border,
+                                                borderColor: Theme.of(context)
+                                                    .accentColor,
+                                                color: Theme.of(context)
+                                                    .accentColor,
+                                                starCount: 5,
+                                              )
+                                            ],
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    flex: 1,
+                                    child: Text(
+                                      user.overall.toStringAsFixed(2),
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w900,
+                                        fontSize: 24.0,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                          );
-                        },
-                        child: Container(
-                          child: Card(
-                            child: Row(
-                              // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  flex: 4,
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(12.0),
-                                    child: Row(
-                                      children: [
-                                        Text(
-                                          '${index + 1}',
-                                          style: TextStyle(
-                                            fontSize: 24.0,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          width: 10.0,
-                                        ),
-                                        user.imgUrl == null
-                                            ? ProfilePlaceholder(radius: 24.0)
-                                            : CustomCircleProfilePic(
-                                                imgUrl: user.imgUrl,
-                                                radius: 24.0,
-                                              ),
-                                        SizedBox(
-                                          width: 20.0,
-                                        ),
-                                        Column(
-                                          crossAxisAlignment:
-                                              CrossAxisAlignment.start,
-                                          children: [
-                                            Text(
-                                              user.name,
-                                              style: TextStyle(
-                                                fontWeight: FontWeight.bold,
-                                                fontSize: 18.0,
-                                              ),
-                                            ),
-                                            SmoothStarRating(
-                                              size: 25,
-                                              rating: user.overall,
-                                              isReadOnly: true,
-                                              filledIconData: Icons.star,
-                                              halfFilledIconData:
-                                                  Icons.star_half,
-                                              defaultIconData:
-                                                  Icons.star_border,
-                                              borderColor:
-                                                  Theme.of(context).accentColor,
-                                              color:
-                                                  Theme.of(context).accentColor,
-                                              starCount: 5,
-                                            )
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: 1,
-                                  child: Text(
-                                    user.overall.toStringAsFixed(2),
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.w900,
-                                      fontSize: 24.0,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
                           ),
-                        ),
-                      );
-                    },
-                    // separatorBuilder: (_, __) {
-                    //   return Divider();
-                    // },
-                    itemCount: snapshot.data.length,
-                  ),
-                );
-              },
+                        );
+                      },
+                      // separatorBuilder: (_, __) {
+                      //   return Divider();
+                      // },
+                      itemCount: snapshot.data.length,
+                    );
+                  },
+                ),
+              ),
             ),
           ],
         ),
